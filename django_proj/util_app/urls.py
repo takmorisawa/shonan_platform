0a619c9eb4ecf7cb461b52206bc4f7fce39a491f
tkyk <tkyk@tkyknoMacBook-Pro.local>
2020-06-09 00:35:47 +0900

    feat: csv file uploader for device

--- django_proj/util_app/urls.py
+++ django_proj/util_app/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 app_name = "util_app"
 urlpatterns = [
-    path('upload/', views.upload, name='upload'),
+    path('upload/report/', views.upload_report, name='upload_report'),
+    path('upload/device/', views.upload_device, name='upload_device'),
+    path('upload/', views.upload_top, name='upload_top'),
 ]
--- django_proj/util_app/views.py
+++ django_proj/util_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
-from rest_app.models import Report
-from rest_app.serializer import ReportSerializer
+from rest_app.models import Report, Device
+from rest_app.serializer import ReportSerializer, DeviceSerializer
 from io import TextIOWrapper, StringIO
 import json
 import csv
 
 
-def upload(request):
-    params = { "breadcrumblist" : [ ["HOME", ""] ] }
+def upload(request, serializer_class, model_class):
+    params = { "breadcrumblist" : [ ["HOME", "/util/upload/"] ] }
 
     if 'csv' in request.FILES:
         form_data = TextIOWrapper(request.FILES['csv'].file, encoding='utf-8')
@@ -20,19 +20,18 @@ def upload(request):
         json_data = json.dumps(tmp_data)
 
         data = json.loads(json_data)
-        serializer = ReportSerializer(data=data, many=True)
+        serializer = serializer_class(data=data, many=True)
         if serializer.is_valid():
-            params["columns"] = header
             params["records"] = data
             params["json"] = json_data
         else:
             params["message"] = "不正な入力ファイルです"
 
     elif "json" in request.POST:
-        serializer = ReportSerializer(data=json.loads(request.POST["json"]), many=True)
+        serializer = serializer_class(data=json.loads(request.POST["json"]), many=True)
         if serializer.is_valid():
             for data in serializer.validated_data:
-                report = Report.objects.create(**data)
+                obj = model_class.objects.create(**data)
             params["message"] = "アップロード完了"
         else:
             params["message"] = "アップロードに失敗しました"
@@ -42,4 +41,20 @@ def upload(request):
     else:
         params["disabled"] = "disabled"
 
+    return params
+
+def upload_report(request):
+    params = upload(request, ReportSerializer, Report)
+    params["breadcrumblist"].insert(1,["Report", ""]);
     return render(request, 'upload.html', params)
+
+def upload_device(request):
+    params = upload(request, DeviceSerializer, Device)
+    params["breadcrumblist"].insert(1,["Device", ""]);
+    return render(request, 'upload.html', params)
+
+def upload_top(request):
+    params = {
+        "breadcrumblist" : [ ["HOME", ""] ],
+        "tables" : [ ["Device", "./device"], ["Report", "./report"] ] }
+    return render(request, 'upload_top.html', params)
