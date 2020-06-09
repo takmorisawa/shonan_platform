509c03a6dd43f1dae6fcd88c9e42b5b4af61bcee
tkyk <tkyk@tkyknoMacBook-Pro.local>
2020-06-10 03:35:07 +0900

    feat: post redirect get

--- django_proj/util_app/urls.py
+++ django_proj/util_app/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 
+from util_app.views import DeviceUploadViewSet, ReportUploadViewSet
 from . import views
 
 app_name = "util_app"
 urlpatterns = [
-    path('upload/report/', views.upload_report, name='upload_report'),
-    path('upload/device/', views.upload_device, name='upload_device'),
+    path('upload/device/process/', DeviceUploadViewSet.upload),
+    url('upload/device/.*', DeviceUploadViewSet.validate),
+
+    path('upload/report/process/', ReportUploadViewSet.upload),
+    url('upload/report/.*', ReportUploadViewSet.validate),
+
     path('upload/', views.upload_top, name='upload_top'),
 ]
--- django_proj/util_app/views.py
+++ django_proj/util_app/views.py
@@ -4,54 +4,93 @@ from rest_app.serializer import ReportSerializer, DeviceSerializer
 from io import TextIOWrapper, StringIO
 import json
 import csv
+import re
 
 
-def upload(request, serializer_class, model_class):
-    params = { "breadcrumblist" : [ ["HOME", "/util/upload/"] ] }
+class Meta(type):
+    def __init__(cls, name, bases, attribute):
+        super(Meta, cls).__init__(name, bases, attribute)
+        validate_detail = getattr(cls, "validate_detail")
+        upload_detail = getattr(cls, "upload_detail")
+        serializer_class = getattr(cls, "serializer_class")
+        model_class = getattr(cls, "model_class")
+        cls.validate = lambda request: validate_detail(request, serializer_class)
+        cls.upload = lambda request : upload_detail(request, serializer_class, model_class)
 
-    if 'csv' in request.FILES:
-        form_data = TextIOWrapper(request.FILES['csv'].file, encoding='utf-8')
 
-        reader = csv.reader(form_data)
-        header = next(reader)
-        tmp_data = []
-        for row in reader:
-            tmp_data.append({k:v for k,v in zip(header, row)})
-        json_data = json.dumps(tmp_data)
+class CsvUploadViewSet(metaclass=Meta):
+    serializer_class = None
+    model_class = None
 
-        data = json.loads(json_data)
-        serializer = serializer_class(data=data, many=True)
-        if serializer.is_valid():
-            params["records"] = data
-            params["json"] = json_data
+    def validate_detail(request, serializer_class):
+
+        path = request.path
+        m = re.match("/util/upload/(.+)/.*", path)
+        target = m.groups()[0] if m else ""
+
+        params = {
+            "breadcrumblist" : [ ["HOME", "/util/upload/"], [target.capitalize(), ""] ],
+            "validate_action" : re.sub("process/.*","", path),
+            "upload_action" : "./process/",
+            "message" : "",
+        }
+        if "success" in path:
+            params["message"] = "アップロード成功"
+        elif "fail" in path:
+            params["message"] = "アップロード失敗"
+
+        m = re.match("/util/upload/(.+)/.*", path)
+
+        if 'csv' in request.FILES:
+            form_data = TextIOWrapper(request.FILES['csv'].file, encoding='utf-8')
+
+            # get json string
+            reader = csv.reader(form_data)
+            header = next(reader)
+            tmp_data = []
+            for row in reader:
+                tmp_data.append({k:v for k,v in zip(header, row)})
+            json_data = json.dumps(tmp_data)
+
+            # get dictionary and serialize
+            data = json.loads(json_data)
+            serializer = serializer_class(data=data, many=True)
+            if serializer.is_valid():
+                params["records"] = data
+                params["json"] = json_data
+            else:
+                params["message"] = "不正な入力ファイルです"
+
+        if "json" in params:
+            params["breadcrumblist"].append(["データチェック", ""])
         else:
-            params["message"] = "不正な入力ファイルです"
+            params["disabled"] = "disabled"
 
-    elif "json" in request.POST:
-        serializer = serializer_class(data=json.loads(request.POST["json"]), many=True)
-        if serializer.is_valid():
-            for data in serializer.validated_data:
-                obj = model_class.objects.create(**data)
-            params["message"] = "アップロード完了"
-        else:
-            params["message"] = "アップロードに失敗しました"
+        return render(request, 'upload.html', params)
 
-    if "json" in params:
-        params["breadcrumblist"].append(["データチェック", ""])
-    else:
-        params["disabled"] = "disabled"
 
-    return params
+    def upload_detail(request, serializer_class, model_class):
 
-def upload_report(request):
-    params = upload(request, ReportSerializer, Report)
-    params["breadcrumblist"].insert(1,["Report", ""]);
-    return render(request, 'upload.html', params)
+        url = "./fail/"
+        if "json" in request.POST:
+            serializer = serializer_class(data=json.loads(request.POST["json"]), many=True)
+            if serializer.is_valid():
+                for data in serializer.validated_data:
+                    obj = model_class.objects.create(**data)
+                url = "./success/"
 
-def upload_device(request):
-    params = upload(request, DeviceSerializer, Device)
-    params["breadcrumblist"].insert(1,["Device", ""]);
-    return render(request, 'upload.html', params)
+        return redirect(url)
+
+
+class DeviceUploadViewSet(CsvUploadViewSet):
+    serializer_class = DeviceSerializer
+    model_class = Device
+
+
+class ReportUploadViewSet(CsvUploadViewSet):
+    serializer_class = ReportSerializer
+    model_class = Report
+
 
 def upload_top(request):
     params = {
